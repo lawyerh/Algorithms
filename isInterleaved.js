41909c2f9c37466a4185566a020a6cff9cae4b6a
lawyerh <lawyerh_01@yahoo.com>
2018-06-01 10:46:19 -0700

    added interleaving strings algo

---
+++ isInterleaved.js
@@ -0,0 +1,25 @@
+function isInterleaved (str1, str2, str3){
+    let charDict = {};
+    let temp = str1.concat(str2);
+    for(let idx=0; idx < temp.length; idx++){
+        if(!charDict[temp[idx]]){
+            charDict[temp[idx]] = 1;
+        }
+        else {
+            charDict[temp[idx]];
+        }
+    }
+    for(let idx = 0; idx < str3.length; idx++) {
+        if(!charDict[str3[idx]]){
+            return false;
+        }
+        else {
+            charDict[str3[idx]]--;
+            if(charDict[str3[idx]] == 0)
+            {
+                charDict[str3[idx]].clear();
+            }
+        }
+    }
+    return true;
+}
